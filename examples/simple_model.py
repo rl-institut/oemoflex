3a3abd2a81fdcb56d742dc9b3bca8354110965ef
jnnr <32454596+jnnr@users.noreply.github.com>
2021-02-23 23:22:26 +0100

    Sketch example model steps

--- examples/simple_model.py
+++ examples/simple_model.py
@@ -10,26 +10,45 @@ from oemoflex.model.datapackage import EnergyDataPackage  # , postprocess
 
 destination = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'simple_model')
 
+# setup default structure
 edp = EnergyDataPackage.setup_default(
     name='simple_model',
     basepath=destination,
     datetimeindex=pd.date_range("1/1/2016", periods=24 * 10, freq="H"),
-    components=['heat-demand'],
-    busses=['heat'],
+    components=[
+        'ch4-boiler',
+        'heat-demand'
+    ],
+    busses=[
+        'ch4',
+        'heat',
+    ],
     regions=['A', 'B'],
     links=['A-B'],
 )
 
-edp.infer_metadata()
+# parametrize
+edp.data['heat-demand']['amount'] = 100
 
+edp.data['ch4-boiler']['capacity'] = [1, 2]
+
+# save to csv
 edp.to_csv_dir(destination)
 
+# add metadata
+edp.infer_metadata()
+
+# create EnergySystem and Model and solve it.
 es = EnergySystem.from_datapackage(os.path.join(destination, 'datapackage.json'))
 
 om = Model(es)
 
 om.solve()
 
+# save EnergySystem with results
+es.results = om.results()
+
+# restore and postprocess
 es_restored = EnergySystem()
 
 es_restored.restore()
