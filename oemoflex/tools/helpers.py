1778a242d16ccff838ea063ca0db1045e79f534d
unknown <julius.meier@gmx.de>
2021-04-16 13:52:34 +0200

    Change yaml loader to FullLoader

--- oemoflex/tools/helpers.py
+++ oemoflex/tools/helpers.py
@@ -3,6 +3,6 @@ import yaml
 
 def load_yaml(file_path):
     with open(file_path, 'r') as yaml_file:
-        yaml_data = yaml.safe_load(yaml_file)
+        yaml_data = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
     return yaml_data
