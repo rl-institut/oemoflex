56e1cc19a6dcb40c73f00523f8ea922979ca0c21
jnnr <32454596+jnnr@users.noreply.github.com>
2021-04-19 11:07:24 +0200

    Obey black

--- setup.py
+++ setup.py
@@ -9,26 +9,26 @@ def read(fname):
 
 
 setup(
-    name='oemoflex',
-    version='0.0.0',
-    description='A flexible model structure for creating and analysing multi-regional'
-                'sector-integrated energysystem models featuring many flexibility options',
-    long_description=read('README.md'),
+    name="oemoflex",
+    version="0.0.0",
+    description="A flexible model structure for creating and analysing multi-regional"
+    "sector-integrated energysystem models featuring many flexibility options",
+    long_description=read("README.md"),
     packages=find_packages(),
     package_data={
-        'oemoflex.model': [
+        "oemoflex.model": [
             "*.yml",
             "*.csv",
             os.path.join("component_attrs", "*.csv"),
         ],
     },
-    package_dir={'oemoflex': 'oemoflex'},
+    package_dir={"oemoflex": "oemoflex"},
     install_requires=[
-        'pyyaml',
-        'pandas',
-        'pyomo<5.6.9',
-        'pyutilib<6.0.0',
-        'oemof.tabular @ git+https://git@github.com/oemof/oemof-tabular@dev#egg=oemof.tabular',
-        'frictionless',
+        "pyyaml",
+        "pandas",
+        "pyomo<5.6.9",
+        "pyutilib<6.0.0",
+        "oemof.tabular @ git+https://git@github.com/oemof/oemof-tabular@dev#egg=oemof.tabular",
+        "frictionless",
     ],
 )
