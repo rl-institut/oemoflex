8ec9ad991e318ee15ec5c04b565c407e4fbf7a3c
jnnr <32454596+jnnr@users.noreply.github.com>
2021-05-03 16:56:45 +0200

    Correct path

--- examples/plotting_data.py
+++ examples/plotting_data.py
@@ -1,21 +1,31 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import oemoflex.tools.plots as plots
 
 # import data and yaml files
-input_data = r'C:\Users\meinm\Documents\Git\oemof-B3-Ergebnisdaten\03_postprocessed\simple_model\sequences\bus\BB-electricity.csv'
-data = pd.read_csv(input_data, header=[0,1,2], parse_dates=[0], index_col=[0])
+here = os.path.abspath(os.path.dirname(__file__))
+input_path = os.path.join(
+    here, "03_postprocessed", "simple_model", "sequences", "bus", "BB-electricity.csv"
+)
+data = pd.read_csv(input_path, header=[0, 1, 2], parse_dates=[0], index_col=[0])
 
 
-fig, ax = plt.subplots(figsize=(12,5))
-data = plots.eng_format(ax, data, 'W', 1000)
+fig, ax = plt.subplots(figsize=(12, 5))
+data = plots.eng_format(ax, data, "W", 1000)
 
-start_date = '2019-12-01 00:00:00'
-end_date = '2019-12-13 23:00:00'
-plots.plot_dispatch(ax = ax, df=data, start_date=start_date, end_date=end_date,
-                    bus_name='BB-electricity', demand_name='BB-electricity-demand')
+start_date = "2019-12-01 00:00:00"
+end_date = "2019-12-13 23:00:00"
+plots.plot_dispatch(
+    ax=ax,
+    df=data,
+    start_date=start_date,
+    end_date=end_date,
+    bus_name="BB-electricity",
+    demand_name="BB-electricity-demand",
+)
 
 # ymin, ymax: es gibt noch Fehlerwerte in den Daten (extrem große kurze Produktion)
-plt.legend(loc='best')
+plt.legend(loc="best")
 plt.tight_layout()
 plt.show()
