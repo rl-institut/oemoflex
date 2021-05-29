5b24284e1b8df0dfa09505b3bbe95b4821b8c982
jnnr <32454596+jnnr@users.noreply.github.com>
2021-05-29 18:17:05 +0200

    Add docstrings

--- oemoflex/model/variations.py
+++ oemoflex/model/variations.py
@@ -2,13 +2,32 @@ import os
 
 
 class VariationGenerator:
+    r"""
+    A simple class that creates and saves several copies of a DataFramePackage given
+    a definition of what to vary.
 
+    Attributes
+    ----------
+
+    base_datapackage : DataFramepackage
+        DataFramePackage to be used as a basis for variations.
+    """
     def __init__(self, datapackage):
 
         self.base_datapackage = datapackage
 
     def create_variations(self, variation_df, destination):
+        r"""
+        Creates variations and stores them in a specified destination.
 
+        Parameters
+        ----------
+        variation_df : pd.DataFrame
+            DataFrame defining the parameter variations.
+
+        destination : path
+            Path to directory where varied DataFramePackages are saved to.
+        """
         assert variation_df.index.name == 'variation_id', \
             "The variations defined in variation_df must be indexed with 'variation_id'"
 
@@ -25,7 +44,22 @@ class VariationGenerator:
             dp.to_csv_dir(variation_dir)
 
     def create_var(self, dp, changes):
+        r"""
+        Creates a single variation of a DataPackage given a Series that defines what to change.
 
+        Parameters
+        ----------
+        dp : DataFramePackage
+            Base DataFramePackage to vary.
+
+        changes : pd.Series
+            Series defining what to change.
+
+        Returns
+        -------
+        _dp : DataFramePackage
+            Altered DataFramePackage.
+        """
         _dp = dp.copy()
 
         changes = changes.to_dict()
