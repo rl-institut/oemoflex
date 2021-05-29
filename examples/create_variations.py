47a01609e780792ff9038bfbcbc5a00714da03be
jnnr <32454596+jnnr@users.noreply.github.com>
2021-05-29 18:19:27 +0200

    Update example

--- examples/create_variations.py
+++ examples/create_variations.py
@@ -1,3 +1,7 @@
+r"""
+Example on how to use the VariationGenerator. To create the base DataFramePackage, run
+simple_model.py first.
+"""
 import os
 import pandas as pd
 from oemoflex.model.variations import VariationGenerator
@@ -6,13 +10,13 @@ from oemoflex.model.datapackage import EnergyDataPackage
 
 here = os.path.abspath(os.path.dirname(__file__))
 preprocessed = os.path.join(here, '01_preprocessed', 'simple_model')
-variation = os.path.join(here, 'variations.csv')
+variation_specs = os.path.join(here, 'variations.csv')
 varied = os.path.join(here, '04_variations')
 
 edp = EnergyDataPackage.from_csv_dir(preprocessed)
 
 vg = VariationGenerator(edp)
 
-var = pd.read_csv(variation, header=[0, 1], index_col=0)
+what_to_vary = pd.read_csv(variation_specs, header=[0, 1], index_col=0)
 
-vg.create_variations(var, varied)
+vg.create_variations(what_to_vary, varied)
