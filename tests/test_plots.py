4fb1bc7a7977307550a4fa49ace94837632e31f6
jnnr <32454596+jnnr@users.noreply.github.com>
2021-09-23 20:08:40 +0200

    Update name of function to pass test

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -6,7 +6,7 @@ def test_group_agg_by_column():
     df = pd.DataFrame({"A": [1, 1, 1], "B": [2, 2, 2]})
     df.rename(columns={"B": "A"}, inplace=True)
 
-    df_agg = plots.group_agg_by_column(df)
+    df_agg = plots._group_agg_by_column(df)
 
     df_agg_default = pd.DataFrame({"A": [3, 3, 3]})
 
