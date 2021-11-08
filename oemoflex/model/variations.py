dd0f2f66e4fcd36ff8c596be16649af00313fcd3
jnnr <32454596+jnnr@users.noreply.github.com>
2021-11-08 18:18:56 +0100

    Add sketch for sensitivity class

--- oemoflex/model/variations.py
+++ oemoflex/model/variations.py
@@ -2,6 +2,47 @@ import copy
 import os
 
 
+class Sensitivity(object):
+    def __init__(self, lb, ub, n):
+        self.lb = lb
+        self.ub = ub
+        self.n = n
+
+    def sanity_check(self):
+        # assert lb, ub.data["component"] exists
+        # assert index same
+        # find the parameters that are different
+        self.get_param()
+        # assert that the difference is larger than eps
+        # assert lb <= ub
+        # n_param is the number parameters that is varied
+        n_param = 0
+
+        return n_param
+
+    def get_param(self):
+        # get the parameters that are varied
+        param = []
+        return param
+
+    def get_samples_oat(self):
+
+        n_param = self.sanity_check()
+
+        # for each param, create n samples
+        samples = []
+
+        return samples
+
+    def get_samples_lhs(self):
+
+        self.sanity_check()
+
+        samples = []
+
+        return samples
+
+
 class VariationGenerator:
     def __init__(self, datapackage):
 
