c254777d09c27efa98050dd0b5a1319a7ee3a7c5
jnnr <32454596+jnnr@users.noreply.github.com>
2021-11-09 18:42:00 +0100

    Elaborate sanity checks

--- oemoflex/model/variations.py
+++ oemoflex/model/variations.py
@@ -12,27 +12,45 @@ class Sensitivity(object):
     def sanity_check(self):
         # assert lb, ub.data["component"] exists
         for edp in [self.lb, self.ub]:
-            assert "component" in edp.data, f"EnergyDatapackage lb or ub does not contain stacked component data."
+            assert (
+                "component" in edp.data
+            ), f"EnergyDatapackage lb or ub does not contain stacked component data."
 
         lb_data = self.lb.data["component"]
 
         ub_data = self.ub.data["component"]
 
         # assert index same
-        assert lb_data.index.equals(ub_data.index), f"Indexes of lb and ub are not the same."
+        assert lb_data.index.equals(
+            ub_data.index
+        ), f"Indexes of lb and ub are not the same."
 
         # assert auxiliary columns are the same
 
         # find the parameters that are different (comparing NaN)
-        # diff = (lb_data != ub_data) | ((lb_data == lb_data) & (ub_data == ub_data))
-        diff = lb_data.compare(ub_data)
+        diff = ~((lb_data == ub_data) | ((lb_data != lb_data) & (ub_data != ub_data)))
+        ## pandas>1.1.0
+        # diff = lb_data.compare(ub_data)
+
+        # Assert that the data is different
+        assert diff.all().all(), f"There is no difference between lb and ub."
+
+        # Assert that the auxiliary columns are the same
+        assert not diff.loc[:, ["carrier", "region", "tech", "type"]].all().all()
+
+        diff = diff.loc[:, "var_value"]
 
         # assert lb <= ub
+        assert (lb_data.loc[diff] < ub_data.loc[diff]).all()
 
         # assert that the difference is larger than eps
+        difference = ub_data.loc[diff] - lb_data.loc[diff]
+        assert (
+            difference > self.eps
+        ).all(), f"The difference between lb and ub is lower than the defined tolerance of {self.eps}"
 
         # n_param is the number parameters that is varied
-        n_param = 0
+        n_param = len(diff)
 
         return n_param
 
