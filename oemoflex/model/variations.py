39022dafaed0a8bac33543507c4f7e8cbf6d3c04
jnnr <32454596+jnnr@users.noreply.github.com>
2021-11-11 21:15:15 +0100

    Clean up sanity checks

--- oemoflex/model/variations.py
+++ oemoflex/model/variations.py
@@ -2,6 +2,30 @@ import copy
 import os
 
 
+def index_same(df_a, df_b):
+    return df_a.index.equals(df_a.index)
+
+
+def data_same(df_a, df_b):
+    return df_a.equals(df_b)
+
+
+def smaller_equal(df_a, df_b):
+    return (df_a < df_b).all()
+
+
+def get_diff(df_a, df_b):
+    ## pandas>1.1.0
+    # diff = lb_data.compare(ub_data)
+    return ~((df_a == df_b) | ((df_a != df_a) & (df_b != df_b)))
+
+
+def diff_larger_eps(df_a, df_b, eps):
+    diff = get_diff(df_a, df_b)
+    df_diff = df_a.loc[diff] - df_b.loc[diff]
+    return abs(df_diff) > eps
+
+
 class Sensitivity(object):
     def __init__(self, lb, ub, n, eps=1e6):
         self.lb = lb
@@ -21,38 +45,25 @@ class Sensitivity(object):
         ub_data = self.ub.data["component"]
 
         # assert index same
-        assert lb_data.index.equals(
-            ub_data.index
-        ), f"Indexes of lb and ub are not the same."
-
-        # assert auxiliary columns are the same
-
-        # find the parameters that are different (comparing NaN)
-        diff = ~((lb_data == ub_data) | ((lb_data != lb_data) & (ub_data != ub_data)))
-        ## pandas>1.1.0
-        # diff = lb_data.compare(ub_data)
+        assert index_same(lb_data, ub_data), f"Indexes of lb and ub are not the same."
 
         # Assert that the data is different
-        assert diff.all().all(), f"There is no difference between lb and ub."
+        assert not data_same(lb_data, ub_data), f"There is no difference between lb and ub."
 
         # Assert that the auxiliary columns are the same
-        assert not diff.loc[:, ["carrier", "region", "tech", "type"]].all().all()
+        AUX_COLUMNS = ["carrier", "region", "tech", "type"]
+        assert data_same(lb_data.loc[:, AUX_COLUMNS], ub_data.loc[:, AUX_COLUMNS])
 
-        diff = diff.loc[:, "var_value"]
+        # find the parameters that are different (comparing NaN)
+        diff = get_diff(lb_data.loc[:, "var_value"], ub_data.loc[:, "var_value"])
 
         # assert lb <= ub
-        assert (lb_data.loc[diff] < ub_data.loc[diff]).all()
+        assert smaller_equal(lb_data.loc[:, "var_value"], ub_data.loc[:, "var_value"])
 
         # assert that the difference is larger than eps
-        difference = ub_data.loc[diff] - lb_data.loc[diff]
-        assert (
-            difference > self.eps
-        ).all(), f"The difference between lb and ub is lower than the defined tolerance of {self.eps}"
 
-        # n_param is the number parameters that is varied
-        n_param = len(diff)
-
-        return n_param
+        assert diff_larger_eps(lb_data, ub_data, self.eps), \
+            f"The difference between lb and ub islower than the defined tolerance of {self.eps}"
 
     def get_param(self):
         # get the parameters that are varied
@@ -61,7 +72,7 @@ class Sensitivity(object):
 
     def get_samples_oat(self):
 
-        n_param = self.sanity_check()
+        self.sanity_check()
 
         # for each param, create n samples
         samples = []
