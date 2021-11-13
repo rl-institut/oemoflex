ca5aa3a06fa8450a1c43537d1aea51c89f56504d
jnnr <32454596+jnnr@users.noreply.github.com>
2021-11-13 15:59:41 +0100

    Use EDPsensitivity in the previous tests

--- tests/test_sensitivity.py
+++ tests/test_sensitivity.py
@@ -1,5 +1,5 @@
 from oemoflex.model.datapackage import EnergyDataPackage
-from oemoflex.model.variations import Sensitivity
+from oemoflex.model.variations import Sensitivity, EDPSensitivity
 import os
 import copy
 import pytest
@@ -8,12 +8,12 @@ here = os.path.dirname(__file__)
 path_edp_lb = os.path.join(here, "_files", "edp_lb")
 path_edp_ub = os.path.join(here, "_files", "edp_ub")
 
-lb = EnergyDataPackage.from_csv_dir(path_edp_lb).data["component"]
-ub = EnergyDataPackage.from_csv_dir(path_edp_ub).data["component"]
+lb = EnergyDataPackage.from_csv_dir(path_edp_lb)
+ub = EnergyDataPackage.from_csv_dir(path_edp_ub)
 
 
 def test_sanity_check():
-    sens = Sensitivity(lb, ub, n=4)
+    sens = EDPSensitivity(lb, ub, n=4)
     sens.sanity_check()
 
 
@@ -22,16 +22,16 @@ def test_sanity_check_not_stacked():
     ub_unstacked = copy.deepcopy(ub)
     lb_unstacked.unstack_components()
     ub_unstacked.unstack_components()
-    sens = Sensitivity(lb_unstacked, ub_unstacked, n=4)
+    sens = EDPSensitivity(lb_unstacked, ub_unstacked, n=4)
     with pytest.raises(AssertionError):
         sens.sanity_check()
 
 
 def test_sanity_check_index_different():
-    lb_unstacked = copy.deepcopy(lb)
-    ub_unstacked = copy.deepcopy(ub)
+    lb.data["component"].reset_index(inplace=True, drop=True)
 
-    sens = Sensitivity(lb, ub, n=4)
+    sens = EDPSensitivity(lb, ub, n=4)
+
     with pytest.raises(AssertionError):
         sens.sanity_check()
 
