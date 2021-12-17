7b3cdbc975fff805d3d847a24604571571fdccb2
jnnr <32454596+jnnr@users.noreply.github.com>
2021-12-17 19:11:23 +0100

    Add some tests

--- tests/test_sensitivity.py
+++ tests/test_sensitivity.py
@@ -1,15 +1,33 @@
+import pandas as pd
+
 from oemoflex.model.datapackage import EnergyDataPackage
-from oemoflex.model.variations import Sensitivity, EDPSensitivity
+from oemoflex.model.variations import Sensitivity, EDPSensitivity, diff_larger_eps, get_diff
 import os
 import copy
 import pytest
 
 here = os.path.dirname(__file__)
+path_lb = os.path.join(here, "_files", "lb.csv")
+path_ub = os.path.join(here, "_files", "ub.csv")
 path_edp_lb = os.path.join(here, "_files", "edp_lb")
 path_edp_ub = os.path.join(here, "_files", "edp_ub")
 
+lb = pd.read_csv(path_lb, header=0)
+ub = pd.read_csv(path_ub, header=0)
 edp_lb = EnergyDataPackage.from_csv_dir(path_edp_lb)
 edp_ub = EnergyDataPackage.from_csv_dir(path_edp_ub)
+
+
+def test_get_diff():
+    assert (get_diff(lb["var_value"], ub["var_value"]) == pd.Series([True, True, False, False])).all()
+
+
+def test_diff_larger_eps():
+    assert diff_larger_eps(lb["var_value"], ub["var_value"], 1e-3).all()
+
+
+def test_init():
+    sens = Sensitivity(lb, ub)
 
 
 def test_sanity_check():
