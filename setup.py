c1d0e5a8d67bb57300ff0046785be68361b16790
MaGering <marie-claire.gering@rl-institut.de>
2023-11-21 18:50:49 +0100

    Fix oemof.tabular to dev

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         "pyyaml",
         "dynaconf",
         "pandas",
-        "oemof.tabular @ git+https://github.com/oemof/oemof-tabular.git@v0.0.4.dev1",
+        "oemof.tabular @ git+https://github.com/oemof/oemof-tabular.git@dev",
         "plotly",
         "frictionless",
         "matplotlib",
